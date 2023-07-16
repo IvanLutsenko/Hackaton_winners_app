da60376db7bc28e98ef7152c9afca3a1721aa002
ivanlutsenko <Ivan.Lutsenko@berekebank.kz>
2023-07-16 17:57:52 +0600

    базовое подключение webview

--- app/src/main/java/com/example/hackathonwinnersapp/MainActivity.kt
+++ app/src/main/java/com/example/hackathonwinnersapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.hackathonwinnersapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hackatonwinnersapp.R
 
@@ -8,5 +11,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setWebview()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setWebview() {
+        val webView: WebView = findViewById(R.id.webview)
+        val address = "http://ya.ru"
+
+        with(webView) {
+            webViewClient = WebViewClient()
+            loadUrl(address)
+            settings.javaScriptEnabled = true
+        }
     }
 }
